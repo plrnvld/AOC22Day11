28249f98fd0a25bd15bc822fc1c36df0180ff40e
Paul Rijneveld <paulrijneveld@gmail.com>
2022-12-20 12:04:37 +0000

    Add Monkey struct

--- main.go
+++ main.go
@@ -1,9 +1,44 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
+type OperationFunc func(old int) int
+type TestFunc func(val int) bool
+
+type Monkey struct {  
+    Num int
+    Items []int
+    Operation OperationFunc
+    Test TestFunc
+    TrueDest int
+    FalseDest int
+}
+
+func New(num int, items []int, operation OperationFunc, test TestFunc, trueDest int, falseDest int) Monkey {  
+    m := Monkey {num, items, operation, test, trueDest, falseDest }
+    return m
+}
+
+func (m Monkey) PrintNumItems() {  
+    fmt.Printf("Mondy %d has %d items\n", m.Num, len(m.Items))
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	readFile, err := os.Open("Example.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fileScanner := bufio.NewScanner(readFile)
+
+	fileScanner.Split(bufio.ScanLines)
+
+	for fileScanner.Scan() {
+		fmt.Println(fileScanner.Text())
+	}
+
+	readFile.Close()
 }
